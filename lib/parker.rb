66ef54c6a7c24a5241d6f302dc509d655f72f0eb
Matt Blewitt <blewitt.matt@gmail.com>
2015-05-16 06:55:51 +0100

    Tidy Up
    * More robust docs
    * DRY up announcement methods

--- lib/parker.rb
+++ lib/parker.rb
@@ -13,6 +13,7 @@ module Parker
       @github ||= Octokit::Client.new(access_token: "#{ENV['GITHUB_TOKEN']}")
     end
 
+    # Establish connection to Hipchat via API
     # @note requires a valid API token for Hipchat (Room or User)
     def hipchat
       @hipchat ||= HipChat::Client.new(ENV['HIPCHAT_TOKEN'], api_version: 'v2')
@@ -36,48 +37,50 @@ module Parker
       github.list_issues("#{ENV['REPO']}", labels: "#{ENV['BUG_LABEL']},#{ENV['P2_LABEL']}")
     end
 
-    # Return a string containing the url, issue number and title for each issue in a collection of issues
+    # Return a string containing the url, issue number, title and how long an issue has been open
+    # for each issue in a collection of issues
     # @param issues [Array] array containing issue objects
     # @return [String]
     def issues_info(issues)
+      html = ""
       issues.each do |i|
-        "Link: #{i.html_url} ##{i.number}: #{i.title}<br>"
+        html += "<a href='#{i.html_url}'><b>##{i.number}</b>: #{i.title}</a> Open for <b>#{open_for_days(i)}</b> days.<br>"
       end
+      html
     end
 
+    # Calculates the time in days an issue has been open for
+    # @param issue [Object] Github issue object
+    # @retun [Integer]
     def open_for_days(issue)
       ((Time.now - issue.created_at).round) / 86_400
     end
 
+    # Generates the body of the hipchat message for priority 1 issues
+    # @return [String] containing html formatted hipchat message
     def p1_announcement
-      if p1_issues.count > 0
-        html += "There are currently <b>#{p1_issues.count}</b> Priority 1 bugs open:<br>"
-        p2_issues.each do |i|
-          html += "<a href='#{i.html_url}'><b>##{i.number}</b>: #{i.title}</a> Open for <b>#{open_for_days(i)}</b> days.<br>"
-        end
-        html
-      else
-        "There are currently <b>#{p1_issues.count}</b> Priority 1 bugs open.<br>"
-      end
+      html = "There are currently <b>#{p1_issues.count}</b> #{ENV['P1_LABEL']} bugs open.<br>"
+      html += issues_info(p1_issues)
+      html
     end
 
+    # Generates the body of the hipchat message for priority 2 issues
+    # @return [String] containing html formatted hipchat message
     def p2_announcement
-      if p2_issues.count > 0
-        html = "<br>There are currently <b>#{p2_issues.count}</b> Priority 2 bugs open:<br>"
-        p2_issues.each do |i|
-          html += "<a href='#{i.html_url}'><b>##{i.number}</b>: #{i.title}</a> Open for <b>#{open_for_days(i)}</b> days.<br>"
-        end
-        html
-      else
-        "<br>There are currently #{p2_issues.count} Priority 2 bugs open."
-      end
+      html = "<br>There are currently <b>#{p2_issues.count}</b> #{ENV['P2_LABEL']} bugs open.<br>"
+      html += issues_info(p2_issues)
+      html
     end
 
+    # Collates the the two announcements into one message for ease of transmission
+    # @return [String]
     def announcements
       Parker.p1_announcement +
         Parker.p2_announcement
     end
 
+    # Sends the collated announcements to the specified hipchat room as Parker, generating a notification 
+    # within that room
     def go!
       Parker.hipchat["#{ENV['HIPCHAT_ROOM']}"].send('Parker',  Parker.announcements, color: 'purple', notify: true)
     end
--- lib/parker/version.rb
+++ lib/parker/version.rb
@@ -1,5 +1,6 @@
 # Parker version constant
 # @version 0.0.1
 module Parker
+  # Version constant
   VERSION = '0.0.1'
 end
