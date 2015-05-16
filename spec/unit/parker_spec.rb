6f08b767459eb38d3e5f82f3f996ac29ab3b8c78
Matt Blewitt <blewitt.matt@gmail.com>
2015-05-16 17:49:33 +0100

    Couple more tests

--- spec/unit/parker_spec.rb
+++ spec/unit/parker_spec.rb
@@ -2,15 +2,14 @@ require 'spec_helper'
 
 describe Parker do
   subject { described_class }
+  let(:issue) { double(html_url: 'http://www.example.com', number: '3124', title: 'It broke', created_at: Time.now - 432_000) }
   describe '#open_for_days' do
-    let(:issue) { double(created_at: Time.now - 432_000) } # 5 days ago
     it 'returns the number of days the issue is open for' do
       expect(subject.open_for_days(issue)).to eq(5)
     end
   end
   describe '#issues_info' do
     let(:issues) { [issue] }
-    let(:issue)  { double(html_url: 'http://www.example.com', number: '3124', title: 'It broke', created_at: Time.now - 432_000) }
     it 'returns a string containing the url of an issue' do
       expect(subject.issues_info(issues)).to include('http://www.example.com')
     end
@@ -22,6 +21,18 @@ describe Parker do
     end
     it 'returns a string containing the number of days an issue has been open for' do
       expect(subject.issues_info(issues)).to include('5')
+    end
+  end
+  describe '#p1_announcement' do
+    let(:p1_issues) { [issue] }
+    it 'returns a string containing the count of P1 issues' do
+      expect(subject.p1_announcement).to include('1')
+    end
+  end
+  describe '#p2_announcement' do
+    let(:p2_issues) { [issue] }
+    it 'returns a string containing the count of P1 issues' do
+      expect(subject.p2_announcement).to include('1')
     end
   end
 end
